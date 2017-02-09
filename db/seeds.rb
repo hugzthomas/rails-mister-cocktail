0ba3a530779ebfee9905dabd5b6ed4e5e48d29fd
hugz thomas <hugues.lam.thomas@gmail.com>
2017-02-09 17:09:12 +0100

    grosse journée

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,28 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+require "json"
+require "open-uri"
+
+Ingredient.destroy_all
+Cocktail.destroy_all
+
+url = "http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+ingredient_serialized = open(url).read
+repos = JSON.parse(ingredient_serialized)
+# TODO: Write a seed to insert 10 posts in the database fetched from the Hacker News API.
+repos["drinks"].each do |ingredient|
+  Ingredient.create(name: ingredient["strIngredient1"])
+end
+
+
+Cocktail.create!(name: "Sex on the beach")
+Cocktail.create!(name: "Old fashioned")
+Cocktail.create!(name: "Whiskey sour")
+Cocktail.create!(name: "L'incontournable Mojito")
+Cocktail.create!(name: "Bloody Mary")
+Cocktail.create!(name: "Lost at sea")
+Cocktail.create!(name: "Mai Tai")
+Cocktail.create!(name: "Screw Driver")
+Cocktail.create!(name: "Sorry not tonight")
+Cocktail.create!(name: "What's my name")
+Cocktail.create!(name: "where is mary")
+Cocktail.create!(name: "coqtail")
